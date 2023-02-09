d333f8aa00eed193fb451787886e7a6aa383b38c
Elena Radu <lena@Air-de-Elena.home>
2023-02-09 17:26:07 +0100

    categories css

--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -1,9 +1,17 @@
 import { categoriesMock } from "../../categoriesMock";
+import './Categories.styles.css';
 
 const Categories = () => {
     return ( 
-        <div>
-            Categories
+        <div className='categories-container'>
+            {categoriesMock ? categoriesMock.map((element, index) =>
+                    <div key={index} className='category-list'>
+                        <div className='category-main'>{element.category}</div>
+                        <div className='category-image'>{element.image}</div>
+                        <div className='category-name'>{element.name}</div>
+                        <div className='category-price'>{element.price}</div>
+                    </div>
+                ) : ''}
         </div>
     );
 }
