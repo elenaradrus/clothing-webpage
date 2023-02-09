31665e6322361719b7107262c82eabca3325973c
Elena Radu <lena@Air-de-Elena.home>
2023-02-09 20:39:43 +0100

    style footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,28 @@
+import './Footer.styles.css';
+
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <div className='footer-container'>
+                <div className='footerInfo'>
+                    <h3 className='footerTitle'>Jeremy Tunner</h3>
+                    <p className='footerText'>Receive 20% off your first purchase</p>
+                </div>
+
+                <div className='footerInfo'>
+                    <h4 className='footerTitle'>Call or text</h4>
+                    <p className='footerText'>We'd love to engage in some good old fashioned conversation. Available M-F 9am-5pm Pacific.</p>
+                    <p>(415) 304-8208</p>
+                </div>
+
+                <div className='footerInfo'>
+                    <h4 className='footerTitle'>Email</h4>
+                    <p className='footerText'>Drop us a line anytime and we'll get back to you within 24 hours.</p>
+                    <p className='footerText'>Contact us</p>
+                </div>
+            </div>
+
+            <div className='footerCopywrite'>&copy; 2009-2023 Jeremy Tunner</div>
         </div>
     );
 }
