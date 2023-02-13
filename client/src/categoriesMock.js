631c47ea6db5766fd7b794f5f5b9400583364c55
Elena Radu <lena@Air-de-Elena.home>
2023-02-13 20:34:25 +0100

    change url for clothing categories

--- client/src/categoriesMock.js
+++ client/src/categoriesMock.js
@@ -1,37 +1,37 @@
 export const categoriesMock = [
     {
         category: 'Shirts',
-        image: 'https://o.remove.bg/downloads/98fa7256-253c-4c8e-9566-3a7515f3716d/instock_m_q123_California_Heathered_Sunrise_Plaid_001_large-removebg-preview.png',
+        image: 'http://localhost:3000/shirts.png',
         name: 'The California',
         price: '$128'
     },
     {
         category: 'Bottoms',
-        image: 'https://o.remove.bg/downloads/b1df60f2-da6b-4701-aaeb-d83903526595/instock_m_q123_PackPant_CoalFleece_001_large-removebg-preview.png',
+        image: 'http://localhost:3000/bottoms.png',
         name: 'The Pack Pant',
         price: '$138'
     },
     {
         category: 'Outwear',
-        image: 'https://o.remove.bg/downloads/d655b9b5-cbd8-4946-98c4-e7b0038f1b67/instock_m_q123_Cavern_Jacket_Navy_Dobby_Grid_001_large-removebg-preview.png',
+        image: 'http://localhost:3000/outwear.png',
         name: 'The Cavern Jacket',
         price: '$188'
     },
     {
         category: 'Knits',
-        image: 'https://o.remove.bg/downloads/abcc8156-490d-488c-aa78-f133002583a7/instock_m_q123_HB_Tee_DawnPatrol_001_large-removebg-preview.png',
+        image: 'http://localhost:3000/knits.png',
         name: 'The Heavy Bag Tee',
         price: '$50'
     }, 
     {
         category: 'Accesories',
-        image: 'https://o.remove.bg/downloads/76da92db-68b1-445c-b677-96afe71f6972/DROP_POINT_1_large-removebg-preview.png',
+        image: 'http://localhost:3000/accesories.png',
         name: 'The Drop Point Knife',
         price: '$68'
     }, 
     {
         category: 'Footwear',
-        image: 'https://o.remove.bg/downloads/d1dfb3ce-f829-4842-91b9-96818d58e3d8/mens_workshop_Q418_trench_boot_whisky_001_large-removebg-preview.png',
+        image: 'http://localhost:3000/footwear.png',
         name: 'The Trench Boot',
         price: '$348'
     }
