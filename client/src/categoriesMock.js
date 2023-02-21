65fd48b5de3f46d696995c91fb2b9444c37a758e
Elena Radu <lena@Air-de-Elena.home>
2023-02-21 10:51:37 +0100

    start collections page

--- client/src/categoriesMock.js
+++ client/src/categoriesMock.js
@@ -1,35 +1,41 @@
 export const categoriesMock = [
     {
+        id: 1,
         category: 'Shirts',
         image: 'http://localhost:3000/shirts.png',
         name: 'The California',
         price: '$128'
     },
     {
+        id: 2,
         category: 'Bottoms',
         image: 'http://localhost:3000/bottoms.png',
         name: 'The Pack Pant',
         price: '$138'
     },
     {
+        id: 3,
         category: 'Outwear',
         image: 'http://localhost:3000/outwear.png',
         name: 'The Cavern Jacket',
         price: '$188'
     },
     {
+        id: 4,
         category: 'Knits',
         image: 'http://localhost:3000/knits.png',
         name: 'The Heavy Bag Tee',
         price: '$50'
     }, 
     {
+        id: 5,
         category: 'Accesories',
         image: 'http://localhost:3000/accesories.png',
         name: 'The Drop Point Knife',
         price: '$68'
     }, 
     {
+        id: 6,
         category: 'Footwear',
         image: 'http://localhost:3000/footwear.png',
         name: 'The Trench Boot',
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Collections from './pages/Collections/Collections';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/collections" element={<Collections />} />
     </Routes>
   </BrowserRouter>
 );
