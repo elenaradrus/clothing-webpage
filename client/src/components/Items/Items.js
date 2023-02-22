03d6b2b467ef0536e0a29d00f57740ee01cb4c3e
Elena Radu <lena@Air-de-Elena.home>
2023-02-22 11:22:15 +0100

    show collection according to id from params

--- client/src/components/Items/Items.js
+++ client/src/components/Items/Items.js
@@ -1,32 +1,18 @@
 import { categoriesMock } from "../../categoriesMock";
 
-const Items = ({id}) => {
+const Items = ({ id }) => {
 
-    console.log('id en items', id)
-
-    const categoriesId = categoriesMock.map( e => e.id)
-
-    for (let i = 0; i < categoriesId.length; i++) {
-        const element = categoriesId[i];  
-        if(id === element) {
-            console.log('los id coinciden')
-        }
-    }
-
-    const renderItem = () =>{
-        const items = [];
-        for (const category of categoriesMock) {
-            for (const item of category.collections) {
-              items.push(<span>{item.name}</span>);
-            }
-          }
-
-        return items;
-    }
+    const categoriesId = categoriesMock.find(category => category.id === id);
+    const collection = categoriesId.collections;
 
     return (
         <div>
-            {renderItem()}
+            {collection ? collection.map((element, index) =>
+                <div key={index}>
+                    <div>{element.name}</div>
+                    <div>{element.price}</div>
+                </div>
+            ) : ''}
         </div>
 
     );
