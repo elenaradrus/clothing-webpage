7b5c96ecb73af39a882f3394260fb23e6fdf8a1e
Elena Radu <lena@Air-de-Elena.home>
2023-02-28 17:04:36 +0100

    start styles for purchase page

--- client/src/components/AddToCart/AddToCart.js
+++ client/src/components/AddToCart/AddToCart.js
@@ -1,9 +1,32 @@
 //import { useParams } from "react-router-dom";
+import './AddToCart.styles.css';
 
 const AddToCart = () => {
     return (
         <div className="addToCart-container">
-            Add To Cart
+            <div className="addToCart-image">
+                Imagen
+            </div>
+            <div className='product-info'>
+                <div className='product-container'>
+                    <h2>The California</h2>
+                    <h1>$128</h1>
+                </div>
+
+                <div className='btn-container'>
+                    <select className='addToCart-select'>
+                        <option>select size</option>
+                        <option>xs - 36</option>
+                        <option>s - 38</option>
+                        <option>m - 40</option>
+                        <option>l - 42</option>
+                        <option>xl - 44</option>
+                        <option>xxl - 46</option>
+                    </select>
+                    <button className='addToCart-button'>add to cart</button>
+                </div>
+
+            </div>
         </div>
     );
 }
