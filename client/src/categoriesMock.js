29758581de2de41f14c21718146fa728da3b1925
Elena Radu <lena@Air-de-Elena.home>
2023-03-11 13:15:25 +0100

    add images for shirt items

--- client/src/categoriesMock.js
+++ client/src/categoriesMock.js
@@ -10,57 +10,81 @@ export const categoriesMock = [
                 id: 11,
                 name: 'The California',
                 image: 'http://localhost:3000/theCalifornia.jpeg',
+                secondImage: 'http://localhost:3000/theCalifornia2.jpeg',
                 thumbnail: 'http://localhost:3000/theCaliforniaModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theCaliforniaLandscape.jpeg',
                 price: '$128',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 12,
                 name: 'The Jack',
                 image: 'http://localhost:3000/theJack.jpeg',
+                secondImage: 'http://localhost:3000/theJack2.jpeg',
                 thumbnail: 'http://localhost:3000/theJackModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theJackLandscape.jpeg',
                 price: '$125',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 13,
                 name: 'The Ledge Shirt',
                 image: 'http://localhost:3000/theLedge.jpeg',
+                secondImage: 'http://localhost:3000/theLedge2.jpeg',
                 thumbnail: 'http://localhost:3000/theLedgeModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theLedgeLandscape.jpeg',
                 price: '$128',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 14,
                 name: 'The Utility Shirt',
                 image: 'http://localhost:3000/theUtility.jpeg',
+                secondImage: 'http://localhost:3000/theUtility2.jpeg',
                 thumbnail: 'http://localhost:3000/theUtilityModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theUtilityLandscape.jpeg',
                 price: '$135',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 15,
                 name: 'The Western Shirt',
                 image: 'http://localhost:3000/theWestern.jpeg',
+                secondImage: 'http://localhost:3000/theWestern2.jpeg',
                 thumbnail: 'http://localhost:3000/theWesternModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theWesternLandscape.jpeg',
                 price: '$148',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 16,
                 name: 'The Division Shirt',
                 image: 'http://localhost:3000/theDivision.jpeg',
+                secondImage: 'http://localhost:3000/theDivision2.jpeg',
                 thumbnail: 'http://localhost:3000/theDivisionModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theDivisionLandscape.jpeg',
                 price: '$128',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 17,
                 name: 'The Point Shirt',
                 image: 'http://localhost:3000/thePoint.jpeg',
+                secondImage: 'http://localhost:3000/thePoint2.jpeg',
                 thumbnail: 'http://localhost:3000/thePointModel.jpeg',
+                landscapeImage: 'http://localhost:3000/thePointLandscape.jpeg',
                 price: '$135',
+                info: 'This garment is made with Organic Cotton'
             },
             {
                 id: 18,
                 name: 'The Core Shirt',
                 image: 'http://localhost:3000/theCore.jpeg',
+                secondImage: 'http://localhost:3000/theCore2.jpeg',
                 thumbnail: 'http://localhost:3000/theCoreModel.jpeg',
+                landscapeImage: 'http://localhost:3000/theCoreLandscape.jpeg',
                 price: '$148',
+                info: 'Jeremy Tunner x the ecology center'
             }
         ]
     },
--- client/src/components/AddToCart/AddToCart.js
+++ client/src/components/AddToCart/AddToCart.js
@@ -9,36 +9,51 @@ const AddToCart = ({ id, itemId }) => {
     const categoriesId = categoriesMock.find(category => category.id === id);
     const collection = categoriesId.collections;
     const getItem = collection.find(e => e.id == itemId)
-    
+
     return (
         <div className="addToCart-container">
-            <div className="addToCart-image">
+
+            <div className='addToCart-product'>
+                <div className="addToCart-image">
+                    <img
+                        src={getItem.secondImage}
+                        title={getItem.name}
+                        alt={getItem.name}
+                    />
+                </div>
+
+                <div className='product-container'>
+                    <div className='product-info'>
+                        <h2>{getItem.name}</h2>
+                        <h1>{getItem.price}</h1>
+                    </div>
+
+                    <div className='btn-container'>
+                        <select className='addToCart-select'>
+                            <option>select size</option>
+                            <option>xs - 36</option>
+                            <option>s - 38</option>
+                            <option>m - 40</option>
+                            <option>l - 42</option>
+                            <option>xl - 44</option>
+                            <option>xxl - 46</option>
+                        </select>
+                        <button className='addToCart-button'>add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className='landscapeImage-container'>
+                <p className='itemInfo'>{getItem.info}</p>
+
                 <img
-                    src={getItem.image}
+                    className='landscapeImage'
+                    src={getItem.landscapeImage}
                     title={getItem.name}
                     alt={getItem.name}
                 />
             </div>
-            <div className='product-info'>
-                <div className='product-container'>
-                    <h2>{getItem.name}</h2>
-                    <h1>{getItem.price}</h1>
-                </div>
 
-                <div className='btn-container'>
-                    <select className='addToCart-select'>
-                        <option>select size</option>
-                        <option>xs - 36</option>
-                        <option>s - 38</option>
-                        <option>m - 40</option>
-                        <option>l - 42</option>
-                        <option>xl - 44</option>
-                        <option>xxl - 46</option>
-                    </select>
-                    <button className='addToCart-button'>add to cart</button>
-                </div>
-
-            </div>
         </div>
     );
 }
