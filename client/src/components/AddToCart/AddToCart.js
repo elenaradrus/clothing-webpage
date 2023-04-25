df127228497093981571a697841f3b11d42e94b7
Elena Radu <lena@Air-de-Elena.home>
2023-04-25 12:03:15 +0200

    style addToCart component for mobile

--- client/src/components/AddToCart/AddToCart.js
+++ client/src/components/AddToCart/AddToCart.js
@@ -101,6 +101,7 @@ const AddToCart = ({ id, itemId }) => {
             <div className='addToCart-product'>
                 <div className="addToCart-image">
                     <img
+
                         loading='lazy'
                         src={item.secondImage}
                         title={item.name}
@@ -129,7 +130,9 @@ const AddToCart = ({ id, itemId }) => {
             </div>
 
             <div className='landscapeImage-container'>
-                <p className='itemInfo'>{item.info}</p>
+                <div className='itemInfo'>
+                    <p>{item.info}</p>
+                </div>
 
                 <img
                     className='landscapeImage'
