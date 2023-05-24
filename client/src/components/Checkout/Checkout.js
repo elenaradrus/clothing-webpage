cfe79497505c9c1d922265f0359d31a303f88a00
Elena Radu <lena@Air-de-Elena.home>
2023-05-24 10:45:38 +0200

    add new font

--- client/src/components/Checkout/Checkout.js
+++ client/src/components/Checkout/Checkout.js
@@ -5,13 +5,13 @@ const Checkout = () => {
     return (
         <div className='checkoutText'>
 
-            <p>This clothing webpage is a personal proyect.</p>
+            <p>This clothing webpage is a personal project.</p>
             <p>
                 If you want to shop the items you saw before you can do that at <a
                     href="https://www.taylorstitch.com/" className='checkoutLink'>Taylor Stitch.</a>
             </p>
             <p>
-                If you want to see the code used to create this proyect, please visit <a
+                If you want to see the code used to create this project, please visit <a
                     href="https://github.com/elenaradrus/clothing-webpage" className='checkoutLink'>
                     my repository.
                 </a>
