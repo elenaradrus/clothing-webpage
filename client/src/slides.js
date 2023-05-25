154e8021cc2f5d8eecb490dccd1acb9fd94bebd7
Elena Radu <lena@Air-de-Elena.home>
2023-05-25 09:10:10 +0200

    add slide images to firestore

--- client/src/slides.js
+++ client/src/slides.js
@@ -1,18 +1,18 @@
 export const slides = [
     {
-        url: "http://localhost:3000/slide1.jpeg", 
+        url: "https://firebasestorage.googleapis.com/v0/b/clothing-webpage.appspot.com/o/slides%2Fslide1.jpeg?alt=media&token=05715e08-75df-441c-aaa6-b0bd725d1420", 
         title: "slide1" 
     },
     {
-        url: "http://localhost:3000/slide2.jpeg", 
+        url: "https://firebasestorage.googleapis.com/v0/b/clothing-webpage.appspot.com/o/slides%2Fslide2.jpeg?alt=media&token=c3d2c55a-574e-47c4-b5ea-b194cf8c20c1", 
         title: "slide2" 
     },
     {
-        url: "http://localhost:3000/slide3.jpeg", 
+        url: "https://firebasestorage.googleapis.com/v0/b/clothing-webpage.appspot.com/o/slides%2Fslide3.jpeg?alt=media&token=57ded960-4612-4eab-97ae-321797459df8", 
         title: "slide3" 
     },
     {
-        url: "http://localhost:3000/slide4.jpeg", 
+        url: "https://firebasestorage.googleapis.com/v0/b/clothing-webpage.appspot.com/o/slides%2Fslide4.jpeg?alt=media&token=65ffd652-496d-4616-bfc5-9ae559c0a409", 
         title: "slide4" 
     }
 ]
